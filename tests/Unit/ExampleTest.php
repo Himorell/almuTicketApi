1d92f7cc1604cc50f9e710cd9cb494c5cdb5c14b
CarmenCruces <karmelacfer@gmail.com>
2023-03-22 08:38:11 +0100

    deleted exampletest

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -9,12 +9,5 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-<<<<<<< HEAD
-    
-=======
-    // public function test_that_true_is_true(): void
-    // {
-    //     $this->assertTrue(true);
-    // }
->>>>>>> dev
+
 }
